848949bf2f44cfea7f80e3ce413e8c581d85aaa1
Divyansh Agrawal <keshav.agr2007@gmail.com>
2025-04-15 22:50:54 +0530

    plswork

--- backend/realestateassistant/wsgi.py
+++ backend/realestateassistant/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'realestateassistant.settings')
 
 application = get_wsgi_application()
+# Handler for Vercel serverless deployment
+def handler(request, **kwargs):
+    return application(request.environ, lambda x, y: [])
